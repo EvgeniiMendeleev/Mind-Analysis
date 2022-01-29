546233f753a2c87e40600b8eb00da2766c6eaf09
EvgenyMendeleev <43094600+EvgeniiMendeleev@users.noreply.github.com>
2022-01-29 13:28:43 +0300

    Low-alpha graphic was added

--- EEG_Graphics/Form1.Designer.cs
+++ EEG_Graphics/Form1.Designer.cs
@@ -33,37 +33,31 @@ namespace EEG_Graphics
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.graphicLowAlpha = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.testName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.graphicLowAlpha)).BeginInit();
             this.SuspendLayout();
             // 
             // graphicLowAlpha
             // 
+            this.graphicLowAlpha.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             chartArea1.Name = "ChartArea1";
             this.graphicLowAlpha.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.graphicLowAlpha.Legends.Add(legend1);
-            this.graphicLowAlpha.Location = new System.Drawing.Point(12, 49);
+            this.graphicLowAlpha.Location = new System.Drawing.Point(17, 67);
             this.graphicLowAlpha.Name = "graphicLowAlpha";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series1.Legend = "Legend1";
             series1.Name = "lowAlphaPoints";
             this.graphicLowAlpha.Series.Add(series1);
-            this.graphicLowAlpha.Size = new System.Drawing.Size(761, 311);
+            this.graphicLowAlpha.Size = new System.Drawing.Size(750, 311);
             this.graphicLowAlpha.TabIndex = 0;
             this.graphicLowAlpha.Text = "chart1";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Low - Alpha:";
             // 
             // button1
             // 
@@ -71,14 +65,45 @@ namespace EEG_Graphics
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(208, 37);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Считать данные с файлов";
+            this.button1.Text = "Считать данные с файла";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnReadDataFromFiles);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название теста:";
+            // 
+            // testName
+            // 
+            this.testName.Location = new System.Drawing.Point(183, 12);
+            this.testName.Name = "testName";
+            this.testName.Size = new System.Drawing.Size(376, 22);
+            this.testName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Low-alpha";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(789, 413);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.testName);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.graphicLowAlpha);
@@ -93,8 +118,10 @@ namespace EEG_Graphics
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart graphicLowAlpha;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox testName;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- EEG_Graphics/Form1.cs
+++ EEG_Graphics/Form1.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace EEG_Graphics
@@ -17,6 +15,38 @@ namespace EEG_Graphics
             InitializeComponent();
         }
 
+        private void OnReadDataFromFiles(object sender, EventArgs e)
+        {
+            graphicLowAlpha.Series[0].Points.Clear();
+            //Менделеев Состояние спокойствия открытые глаза
+            if (string.IsNullOrEmpty(testName.Text))
+            {
+                MessageBox.Show("Нет имя теста", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string filePath = $"..\\Tests\\{testName.Text}.txt";
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Теста нет в папке", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); ;
+                return;
+            }
 
+            StreamReader streamReader = new StreamReader(File.Open(filePath, FileMode.Open));
+
+            while (!streamReader.EndOfStream)
+            {
+                string fileLine = streamReader.ReadLine();
+                string[] testData = Array.ConvertAll(fileLine.Split('='), new Converter<string, string>(str => str.Trim(' ')));
+
+                string time = testData[1].Split(' ')[1].Trim(' ');
+                dynamic jsonObject = JObject.Parse(testData[0]);
+                
+                if (jsonObject.eegPower == null) continue;
+
+                graphicLowAlpha.Series[0].Points.AddXY(time, (int)jsonObject.eegPower["lowAlpha"]);
+            }
+
+            streamReader.Close();
+        }
     }
 }
