d06a1f319d6bf8b46c29daf4de5fc0e5ff6ca057
MrSpooky <43094600+EvgeniiMendeleev@users.noreply.github.com>
2023-01-26 23:43:28 +0300

    Рефакторинг кода и гибкое добавление модулей

--- EEG_Graphics/Form1.cs
+++ EEG_Graphics/Form1.cs
@@ -1,44 +1,28 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Windows.Forms.DataVisualization.Charting;
 using System.Windows.Forms;
 using NeuroTGAM;
-using MindFileSystem;
+using CsvHelper;
 using System.Drawing;
+using System.Globalization;
 
 namespace EEG_Graphics
 {
     //Общие задачи
-    //TODO: Попробовать реализовать вывод графиков ЭЭГ на печать.
     //TODO: Добавить вкладку "О программе"
-    //TODO: Сделать так, чтобы после старта записи данных с нейинтерфейса можно было просмотреть настройки записи, но чтобы нельзя было менять их значения.
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        private delegate void DynamicChartDisplay(EEG_Title chartName, DataPoint point);
-
-        private uint _seconds = 0;
-        private NeuroDeviceTGAM _neurodevice;
+        private delegate void DynamicChartDisplay(BrainInfo brainInfo);
+        private NeuroModuleBase _neurodevice;
         private BrainCharts _brainCharts;
+        private uint _seconds = 0;
 
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
-            _neurodevice = new NeuroDeviceTGAM();
-            _neurodevice.ShowBrainData += DisplayDataToGraphics;
-            _brainCharts = new BrainCharts(new Dictionary<EEG_Title, Chart>()
-            {
-                [EEG_Title.Low_Alpha] = graphicLowAlpha,
-                [EEG_Title.High_Alpha] = graphicHighAlpha,
-                [EEG_Title.Low_Beta] = graphicLowBeta,
-                [EEG_Title.High_Beta] = graphicHighBeta,
-                [EEG_Title.Low_Gamma] = graphicLowGamma,
-                [EEG_Title.High_Gamma] = graphicHighGamma,
-                [EEG_Title.Theta] = graphicTheta,
-                [EEG_Title.Delta] = graphicDelta,
-                [EEG_Title.Attention] = graphicAttention,
-                [EEG_Title.Meditation] = graphicMeditation
-            });
+            _neurodevice = new NeuroConnector();
+            _brainCharts = new BrainCharts();
+            _neurodevice.OnBrainInfoReceived += DisplayDataToGraphics;
             UserControlSystem.GetSystem().Disable(btnStopRecord);
         }
 
@@ -46,7 +30,7 @@ namespace EEG_Graphics
         {
             try
             {
-                _neurodevice.ConnectToConnector();
+                _neurodevice.Connect();
             }
             catch
             {
@@ -55,99 +39,90 @@ namespace EEG_Graphics
             }
             _brainCharts.ClearSerieOnCharts(0);
             UserControlSystem.GetSystem().Disable(btnStartRecord).Enable(btnStopRecord).Disable(btnLoadFirstFile).Disable(groupRecordSettings)
-                .Disable(btnLoadSecondFile).Disable(btnClearAllCharts);
+                .Disable(btnClearAllCharts);
         }
 
         private void StopToReadDataFromNeurodevice(object sender, EventArgs e)
         {
-            if (!_neurodevice.AreDataReading)
+            if (!_neurodevice.AreDataReading())
             {
                 MessageBox.Show("Соединение с ThinkGear Connector не установлено.", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            _neurodevice.DisconnectFromConnector();
+            _neurodevice.CloseConnection();
             _seconds = 0;
-            UserControlSystem.GetSystem()
-                .Disable(btnStopRecord)
-                .Enable(btnStartRecord)
-                .Enable(btnLoadFirstFile)
-                .Enable(btnLoadSecondFile)
-                .Enable(groupRecordSettings)
-                .Enable(btnClearAllCharts);
+            UserControlSystem.GetSystem().Disable(btnStopRecord).Enable(btnStartRecord).Enable(btnLoadFirstFile)
+                .Enable(groupRecordSettings).Enable(btnClearAllCharts);
         }
 
         private void OnChangedValueInSaveMindRecord(object sender, EventArgs e) => btnChangeSavePath.Enabled = !btnChangeSavePath.Enabled;
         private void OnChangedValueInMaxGraphPoints(object sender, EventArgs e) => numMaxChartPoints.Enabled = !numMaxChartPoints.Enabled;
 
-        //TODO: Попробовать реализавать систему, которая бы позволяла добавлять сколько угодно серий точек на график для анализа более чем трёх графиков.
-        private void UploadFirstBrainDataFile(object sender, EventArgs e) => DisplayBrainCharts(1);
-        private void UploadSecondBrainDataFile(object sender, EventArgs e) => DisplayBrainCharts(2);
+        private void UploadBrainFile(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.Filter = "Csv files (*.csv) | *.csv";
+            if (OPF.ShowDialog() != DialogResult.OK) return;
+
+            int serieNumber = Convert.ToInt32(serieNumericUp.Value);
+            _brainCharts.ClearSerieOnCharts(serieNumber);
+
+            using (CsvReader csvReader = new CsvReader(File.OpenText(OPF.FileName), CultureInfo.CurrentCulture))
+            {
+                var brainInfos = csvReader.GetRecords<BrainInfo>();
+                foreach (var brainInfo in brainInfos) _brainCharts.DisplayBrainInfo(brainInfo, serieNumber);
+            }
+
+            _brainCharts.ScaleCharts();
+            OPF.Dispose();
+            UserControlSystem.GetSystem().Enable(btnClearAllCharts);
+        }
 
         private void SaveFilePathOfCurrentMindRecord(object sender, EventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Mind Files (*.mind) | *.mind";
+            saveFileDialog.Filter = "Csv files (*.csv) | *.csv";
             if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
 
             if (saveFileDialog.FileName.Length > 3) fullFilePathText.Text = Path.GetFullPath(saveFileDialog.FileName);
             else MessageBox.Show("Имя файла слишком маленькое!", "Ошибка сохранения", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        void DisplayBrainCharts(int seriesNumber)
-        {
-            OpenFileDialog OPF = new OpenFileDialog();
-            OPF.Filter = "Mind Files (*.mind) | *.mind";
-            if (OPF.ShowDialog() != DialogResult.OK) return;
-
-            _brainCharts.ClearSerieOnCharts(seriesNumber);
-            MindFileReader mindFile = new MindFileReader(OPF.FileName);
-            _brainCharts.DisplayMindFileOnCharts(mindFile, seriesNumber);
-
-            OPF.Dispose();
-            mindFile.Close();
-
-            UserControlSystem.GetSystem().Enable(btnClearAllCharts);
-        }
-
         private void ClearAllGraphics(object sender, EventArgs e) => _brainCharts.ClearAllSeries();
-
-        private void DisplayDataToGraphics(Dictionary<EEG_Title, double> currentBrainData)
+        private void ClearGraphicsWithSerie(object sender, EventArgs e)
         {
-            var brainKeys = currentBrainData.Keys;
-            foreach (EEG_Title brainKey in brainKeys)
-            {
-                object[] DisplaingPointParams = new object[] { brainKey, new DataPoint(_seconds, currentBrainData[brainKey]) };
-                BeginInvoke(new DynamicChartDisplay(DisplayPointToDynamicGraphic), DisplaingPointParams);
-            }
-
-            _seconds++;
-            //TODO: Попробовать переписать сохранение данных c нейроинтерфейса с учётом новой библиотеки (де)сериализации данных в виде JSON строк.
-            if (chkSaveRecordData.Checked) using (FileStream file = new FileStream(fullFilePathText.Text, FileMode.OpenOrCreate))
-                {
-                    file.Seek(0, SeekOrigin.End);
-                    using (StreamWriter writer = new StreamWriter(file))
-                    {
-                        writer.WriteLine($"{EEG_Title.Attention}={currentBrainData[EEG_Title.Attention]}, {EEG_Title.Meditation}={currentBrainData[EEG_Title.Meditation]}," +
-                            $"{EEG_Title.Low_Alpha}={currentBrainData[EEG_Title.Low_Alpha]}, {EEG_Title.High_Alpha}={currentBrainData[EEG_Title.High_Alpha]}," +
-                            $"{EEG_Title.Low_Gamma}={currentBrainData[EEG_Title.Low_Gamma]}, {EEG_Title.High_Gamma}={currentBrainData[EEG_Title.High_Gamma]}," +
-                            $"{EEG_Title.Low_Beta}={currentBrainData[EEG_Title.Low_Beta]}, {EEG_Title.High_Beta}={currentBrainData[EEG_Title.High_Beta]}," +
-                            $"{EEG_Title.Theta}={currentBrainData[EEG_Title.Theta]}, {EEG_Title.Delta}={currentBrainData[EEG_Title.Delta]}:{_seconds}");
-                    }
-                }
+            int serieNumber = Convert.ToInt32(serieNumericUp.Value);
+            _brainCharts.ClearSerieOnCharts(serieNumber);
         }
 
-        void DisplayPointToDynamicGraphic(EEG_Title eegTitle, DataPoint point)
+        private void DisplayDataToGraphics(BrainInfo currentBrainData)
         {
-            if(eegTitle == EEG_Title.Attention)
+            currentBrainData.second = _seconds;
+            BeginInvoke(new DynamicChartDisplay(DisplayPointToDynamicGraphic), currentBrainData);
+            _seconds++;
+
+            if (chkSaveRecordData.Checked) return;
+            using (FileStream file = new FileStream(fullFilePathText.Text, FileMode.OpenOrCreate))
             {
-                attentionLevelChart.Series[0].Points.Clear();
-                if (point.YValues[0] > 0 && point.YValues[0] <= 50) attentionLevelChart.Series[0].Color = Color.Red;
-                else if (point.YValues[0] > 50 && point.YValues[0] <= 80) attentionLevelChart.Series[0].Color = Color.Yellow;
-                else if (point.YValues[0] > 80) attentionLevelChart.Series[0].Color = Color.Green;
-                attentionLevelChart.Series[0].Points.AddXY(1.0f, point.YValues[0]);
+                file.Seek(0, SeekOrigin.End);
+                using (CsvWriter csvWriter = new CsvWriter(new StreamWriter(file), CultureInfo.CurrentCulture))
+                {
+                    csvWriter.WriteRecord(currentBrainData);
+                }
             }
-            if (numMaxChartPoints.Value < _brainCharts.PointsCount(eegTitle, 0)) _brainCharts.DeletePoint(eegTitle, 0, 0);
-            _brainCharts.AddPoint(eegTitle, 0, point);
+        }
+
+        void DisplayPointToDynamicGraphic(BrainInfo brainInfo)
+        {
+            attentionLevelChart.Series[0].Points.Clear();
+            if (brainInfo.attention > 0 && brainInfo.attention <= 50) attentionLevelChart.Series[0].Color = Color.Red;
+            else if (brainInfo.attention > 50 && brainInfo.attention <= 80) attentionLevelChart.Series[0].Color = Color.Yellow;
+            else if (brainInfo.attention > 80) attentionLevelChart.Series[0].Color = Color.Green;
+            attentionLevelChart.Series[0].Points.AddXY(1.0f, brainInfo.attention);
+
+            if (numMaxChartPoints.Value < _brainCharts.DynamicChartPointsCount) _brainCharts.DeletePointOnCharts(0, 0);
+            _brainCharts.DisplayBrainInfo(brainInfo);
+            _brainCharts.ScaleCharts();
         }
     }
 }
---
+++ EEG_Graphics/NeuroModuleBase.cs
@@ -0,0 +1,36 @@
+﻿using NeuroTGAM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EEG_Graphics
+{
+    public abstract class NeuroModuleBase
+    {
+        /// <summary>
+        /// Делегат, позволяющий работать со всеми данными нейрогарнитуры, считанные на текущий момент.
+        /// </summary>
+        /// <param name="currentBrainInfo">Буфер со всеми данными нейрогарнитуры на текущую секунду.</param>
+        public delegate void BrainDataHandler(BrainInfo currentBrainInfo);
+        /// <summary>
+        /// Событие, позволяющее отображать все данные с нейрогарнитуры на экран.
+        /// </summary>
+        public BrainDataHandler OnBrainInfoReceived;
+        protected Thread _readingThread;
+
+        public NeuroModuleBase()
+        {
+            _readingThread = new Thread(ReadingThread);
+            _readingThread.IsBackground = true;
+        }
+
+        public abstract void Connect();
+        public abstract bool AreDataReading();
+        public abstract BrainInfo GetCurrentBrainData();
+        public abstract void CloseConnection();
+        protected abstract void ReadingThread();
+    }
+}
