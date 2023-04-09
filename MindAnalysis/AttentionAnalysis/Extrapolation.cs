10332d76410ebad35a6a7a9faa5c537676390cdb
EvgenyMendeleev <43094600+EvgeniiMendeleev@users.noreply.github.com>
2023-04-09 21:58:55 +0300

    Добавление функционала - Добавлен "Построитель тренда"; - Убран график для второго испытуемого; - Добавлены графики: тренд испытуемого, сглаженная запись.

---
+++ MindAnalysis/AttentionAnalysis/Extrapolation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace AttentionAnalysis
+{
+    internal static class Extrapolation
+    {
+        public static (double k, double b) LinearRegression(DataPoint[] inputPoints)
+        {
+            double k = 0, b = 0;
+
+            int n = inputPoints.Length;
+            double sum_yt = inputPoints.Sum(point => point.YValues[0] * point.XValue);
+            double sum_y = inputPoints.Sum(point => point.YValues[0]);
+            double sum_t = inputPoints.Sum(point => point.XValue);
+            double sum_t_pow = inputPoints.Sum(point => Math.Pow(point.XValue, 2));
+
+            //k = (n * sum_yt - sum_y * sum_t) / (n * sum_t_pow - Math.Pow(sum_t, 2));
+            k = (sum_y * sum_t - n * sum_yt) / (Math.Pow(sum_t, 2) - n * sum_t_pow);
+            b = (sum_y - k * sum_t) / n;
+
+            return (k, b);
+        }
+
+        public static (double A, double B) PowerRegression(DataPoint[] inputPoints)
+        {
+            List<DataPoint> logPoints = new List<DataPoint>(inputPoints);
+            logPoints.ForEach(point =>
+            {
+                point.XValue = Math.Log10(point.XValue);
+                point.YValues[0] = Math.Log10(point.YValues[0]);
+            });
+
+            (double A, double B) = LinearRegression(logPoints.ToArray());
+            return (A, Math.Pow(10, B));
+        }
+
+        public static (double a0, double a1) GiperbolicRegression(DataPoint[] inputPoints)
+        {
+            double n = inputPoints.Length;
+            double sumY = inputPoints.Sum(point => point.YValues[0]);
+            double sumX = inputPoints.Sum(point => 1.0d / point.XValue);
+            double sumYX = inputPoints.Sum(point => point.YValues[0] / point.XValue);
+            double sumX_Pow = inputPoints.Sum(point => 1.0d / Math.Pow(point.XValue, 2));
+
+            double a1 = (sumY * sumX - n * sumYX) / (Math.Pow(sumX, 2) - n * sumX_Pow);
+            double a0 = (sumY - a1 * sumX) / n;
+
+            return (a0, a1);
+        }
+    }
+}
--- MindAnalysis/Form1.cs
+++ MindAnalysis/Form1.cs
@@ -6,32 +6,35 @@ using CsvHelper;
 using System.Drawing;
 using System.Globalization;
 using CsvHelper.Configuration;
-using System.Collections.Generic;
-using MindLinkAnalyze;
 using System.Windows.Forms.DataVisualization.Charting;
-using Microsoft.ML;
 using AttentionModel;
 using System.Linq;
+using MindAnalysis.Forms;
+using AttentionAnalysis;
 
-namespace EEG_Graphics
+namespace MindAnalysis
 {
     //Общие задачи
-    //TODO: Добавить вкладку "О программе"
+    //TODO: [Опционально]. Добавить вкладку "О программе".
+    //TODO: Добавить мастер построения трендов с сглаживанием.
+    //TODO: Добавить оценки качества построенной модели.
+    //TODO: Добавить параметры (цепной и базисный темп/прирост) изменения внимания.
+    //TODO: Решить проблему с "резиновым интерфейсом". Расположение и размер элементов меняется при разработке на мониторах с разными диагоналями.
     public partial class MainForm : Form
     {
         private delegate void DynamicChartDisplay(BrainInfo brainInfo);
         private Neurointerface _neurodevice;
-        private AttentionChart _chartAttention;
-        private uint _seconds = 0;
         private DateTime _startTime;
 
         public MainForm()
         {
             InitializeComponent();
             _neurodevice = new Neurointerface();
-            _neurodevice.OnBrainInfoReceived += (BrainInfo brainInfo) => Invoke(new DynamicChartDisplay(DisplayPointToDynamicGraphic), brainInfo);
-            _chartAttention = new AttentionChart(chartAttention);
-            Console.WriteLine(DateTime.Now);
+            _neurodevice.OnBrainInfoReceived += (BrainInfo brainInfo) =>
+            {
+                Invoke(new DynamicChartDisplay(DisplayPointToDynamicGraphic), brainInfo);
+                if (chkSaveRecordData.Checked) SaveBrainInfoToFile(brainInfo);
+            };
         }
 
         private void StartToReadDataFromNeurodevice(object sender, EventArgs e)
@@ -46,7 +49,7 @@ namespace EEG_Graphics
                 MessageBox.Show("Ошибка подключения к нейроинтерфейсу", "Ошибка соединения!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            _chartAttention.ClearSerie(0);
+            chartAttention.Series[0].Points.Clear();
             UserControlSystem.GetSystem().Disable(btnStartRecord).Enable(btnStopRecord);
         }
 
@@ -58,7 +61,6 @@ namespace EEG_Graphics
                 return;
             }
             _neurodevice.CloseConnection();
-            _seconds = 0;
             UserControlSystem.GetSystem().Disable(btnStopRecord).Enable(btnStartRecord);
         }
 
@@ -73,14 +75,14 @@ namespace EEG_Graphics
             using (OpenFileDialog OPF = new OpenFileDialog() { Filter = "Csv files (*.csv) | *.csv" })
             {
                 if (OPF.ShowDialog() != DialogResult.OK) return;
-                _chartAttention.ClearSerie(serie);
+                chartAttention.Series[serie].Points.Clear();
                 CsvConfiguration csvConfig = new CsvConfiguration(CultureInfo.CurrentCulture) { HasHeaderRecord = false };
                 using (CsvReader csvReader = new CsvReader(File.OpenText(OPF.FileName), csvConfig))
                 {
                     var brainInfos = csvReader.GetRecords<BrainInfo>();
                     foreach (var brainInfo in brainInfos)
                     {
-                        _chartAttention.DisplayBrainInfo(brainInfo, serie);
+                        chartAttention.Series[serie].Points.AddXY(brainInfo.Second.ToString(), brainInfo.Attention);
                     }
                 }
             }
@@ -96,14 +98,77 @@ namespace EEG_Graphics
             else MessageBox.Show("Имя файла слишком маленькое!", "Ошибка сохранения", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        private void ClearExperiment(object sender, EventArgs e) => _chartAttention.ClearSerie(0);
-        private void ClearFirstLoadedFile(object sender, EventArgs e) => _chartAttention.ClearSerie(1);
-        private void ClearSecondLoadedFile(object sender, EventArgs e) => _chartAttention.ClearSerie(2);
+        private void BuildTrendOnChart(object sender, EventArgs e)
+        {
+            TrendBuilder trendBuilder = new TrendBuilder();
+            if (trendBuilder.ShowDialog() != DialogResult.Yes) return;
+            trendBuilder.Dispose();
+
+            TrendParams trendParams = trendBuilder.GetTrendParams();
+            if (chartAttention.Series["Загруженные данные"].Points.Count == 0)
+            {
+                MessageBox.Show("Данные не были загружены в программу!", "Ошибка построения тренда", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DataPoint[] points = chartAttention.Series["Загруженные данные"].Points.ToArray();
+            switch (trendParams.trendSmoothingMode)
+            {
+                case TrendSmoothingMode.MovingAverage:
+                    break;
+                case TrendSmoothingMode.ExponentialSmoothing:
+                    points = DataSmoothing.ExponentialSmoothing(points, (float)trendParams.smoothingParam);
+                    break;
+            }
+
+            int pointsCount = chartAttention.Series["Загруженные данные"].Points.Count;
+            DataPoint[] copyPoints = new DataPoint[points.Length];
+            for (int i = 0; i < pointsCount; i++)
+            {
+                copyPoints[i] = points[i].Clone();
+                copyPoints[i].XValue = i + 1;
+                copyPoints[i].YValues[0] = points[i].YValues[0];
+            }
+
+            if (trendParams.trendSmoothingMode != TrendSmoothingMode.None)
+            {
+                chartAttention.Series["Сглаженная запись"].Points.Clear();
+                for(int i = 0; i < pointsCount; i++) chartAttention.Series["Сглаженная запись"].Points.AddXY(points[i].XValue, copyPoints[i].YValues[0]);
+            }
+
+            chartAttention.Series["Тренд испытуемого"].Points.Clear();
+            switch (trendParams.trendMode)
+            {
+                case TrendMode.Linear:
+                    (double k, double b) = Extrapolation.LinearRegression(copyPoints);
+                    for (int i = 0; i < pointsCount; i++) chartAttention.Series["Тренд испытуемого"].Points.AddXY(points[i].XValue, k * i + b);
+                    break;
+                case TrendMode.Giperbolic:
+                    (double a0, double a1) = Extrapolation.GiperbolicRegression(copyPoints);
+                    for (int i = 1; i < pointsCount; i++) chartAttention.Series["Тренд испытуемого"].Points.AddXY(points[i].XValue, a0 + a1 / i);
+                    break;
+                case TrendMode.Power:
+                    (double A, double B) = Extrapolation.PowerRegression(copyPoints);
+                    if (A.Equals(double.NaN) || B.Equals(double.NaN))
+                    {
+                        MessageBox.Show("Невозможно построить тренд: необходимо сгладить исходные данные.", "Ошибка построения тренда", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        for (int i = 2; i < chartAttention.Series.Count; i++) chartAttention.Series[i].Points.Clear();
+                        return;
+                    }
+                    for (int i = 1; i < pointsCount; i++) chartAttention.Series["Тренд испытуемого"].Points.AddXY(points[i].XValue, B * Math.Pow(i, A));
+                    break;
+            }
+        }
+
+        private void ClearExperiment(object sender, EventArgs e) => chartAttention.Series[0].Points.Clear();
+        private void ClearFirstLoadedFile(object sender, EventArgs e) => chartAttention.Series[1].Points.Clear();
 
         private void ClearAllGraphics(object sender, EventArgs e)
         {
-            _chartAttention.ClearAllSeries();
-            return;
+            foreach (Series serie in chartAttention.Series)
+            {
+                serie.Points.Clear();
+            }
         }
 
         void DisplayPointToDynamicGraphic(BrainInfo brainInfo)
@@ -112,18 +177,16 @@ namespace EEG_Graphics
             elapsedTime = new TimeSpan(elapsedTime.Hours, elapsedTime.Minutes, elapsedTime.Seconds);
             brainInfo.Second = elapsedTime;
 
-            chartAttentionLevel.Series[0].Points.Clear();
-            if (brainInfo.Attention > 0 && brainInfo.Attention <= 50) chartAttentionLevel.Series[0].Color = Color.Red;
-            else if (brainInfo.Attention > 50 && brainInfo.Attention <= 80) chartAttentionLevel.Series[0].Color = Color.Yellow;
-            else if (brainInfo.Attention > 80) chartAttentionLevel.Series[0].Color = Color.Green;
-            chartAttentionLevel.Series[0].Points.AddXY(1.0f, brainInfo.Attention);
+            if (numMaxChartPoints.Value < chartAttention.Series[0].Points.Count)
+            {
+                chartAttention.Series[0].Points.RemoveAt(0);
+                chartAttention.ResetAutoValues();
+            }
+            chartAttention.Series[0].Points.AddXY(brainInfo.Second.ToString(), brainInfo.Attention);
+        }
 
-            if (numMaxChartPoints.Value < _chartAttention.DynamicChartPointsCount) _chartAttention.DeletePoint(0, 0);
-            _chartAttention.DisplayBrainInfo(brainInfo);
-
-            _seconds++;
-
-            if (!chkSaveRecordData.Checked) return;
+        private void SaveBrainInfoToFile(BrainInfo brainInfo)
+        {
             using (FileStream file = new FileStream(fullFilePathText.Text, FileMode.OpenOrCreate, FileAccess.Write))
             {
                 file.Seek(0, SeekOrigin.End);
